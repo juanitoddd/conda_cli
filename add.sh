d6e6ab04b05b997dca0f8dbbc446337c353c9ad3
Juan David <juan.diaz@23degrees.io>
2021-09-05 15:41:29 +0200

    UPDATE

--- add.sh
+++ add.sh
@@ -3,23 +3,27 @@ if [ -z "$1" ]
 then
     echo "Missing argument"
 else
-    echo "Creating new Project $1"
-    
+    echo "== Creating new Project $1"\n\n
     # Git Repo Set Up
     PROJECTS_PATH="/home/juan/dev/python_projects"
     cd $PROJECTS_PATH && gh repo create "$1" --private -y    
     PROJECT_PATH="$PROJECTS_PATH/$1"    
     cd $PROJECT_PATH && echo "# $1 Project" > README.md
+    echo "== Git Repo $1"\n\n
 
     # Conda Env
     conda create --name "$1" -y
     eval "$(conda shell.bash hook)"   
     conda activate "$1"
+    
     cp "$PROJECTS_PATH/cli/requirements.txt" "$PROJECTS_PATH/$1/"
-    conda install -f "$PROJECTS_PATH/utils/requirements.txt"
+    cp "$PROJECTS_PATH/cli/.gitignore" "$PROJECTS_PATH/$1/"
+    
+    conda install -y --file "$PROJECTS_PATH/$1/requirements.txt" -c conda-forge
     python -m ipykernel install --user --name=$1
-    echo "Conda Enviroment $1 created"
-        
+    echo "== Conda Enviroment $1 created"\n\n
+    conda info    
+
     cd $PROJECT_PATH && git add .
     cd $PROJECT_PATH && git commit -am 'INITIAL commit'
     cd $PROJECT_PATH && git push -u origin master
--- remove.sh
+++ remove.sh
@@ -3,12 +3,14 @@ if [ -z "$1" ]
 then
     echo "Missing argument"
 else
+    PROJECTS_PATH="/home/juan/dev/python_projects"
+    PROJECT_PATH="$PROJECTS_PATH/$1"
     echo "Removing Project $1"
     eval "$(conda shell.bash hook)"
     conda activate base
     conda env remove --name "$1"
     jupyter kernelspec uninstall "$1"         
     gh delete "juanitoddd/$1"
-    
+    sudo rm -r $PROJECT_PATH
     cd $PROJECT_PATH
 fi
